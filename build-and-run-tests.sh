24fe3deacaa9fc16d01102e8cfaf30e1ba38b875
Trevor Dolby <trevor.dolby@ibm.com>
2023-09-17 21:35:36 -0500

    Make connection parameters configurable
    Signed-off-by: Trevor Dolby <trevor.dolby@ibm.com>

--- build-and-run-tests.sh
+++ build-and-run-tests.sh
@@ -11,8 +11,18 @@ set -e
 echo ""
 echo "================ Building and running JDBCApplication tests"
 echo ""
-ibmint deploy --input-path $PWD --output-work-directory $PWD/PostgresTestWorkDir --project JDBCApplication --project JDBCApplicationJava --project JDBCApplication_ComponentTest
-IntegrationServer -w $PWD/PostgresTestWorkDir --no-nodejs --start-msgflows no --test-project JDBCApplication_ComponentTest
+
+# We have to copy the work directory out to avoid issues with ibmint, but this
+# also reduces the likelihood of accidentally checking in user/pw information.
+cp -r $PWD/PostgresTestWorkDir /tmp/PostgresTestWorkDir
+mqsisetdbparms -w /tmp/PostgresTestWorkDir -n jdbc::postgres -u postgres -p ${POSTGRES_PASSWORD}
+
+# Fix up the postgres host and port to work in containers
+sed -i "s/localhost/${POSTGRES_HOST}/g" /tmp/PostgresTestWorkDir/run/DemoPolicies/PostgresJDBC.policyxml
+sed -i "s/5432/${POSTGRES_PORT}/g" /tmp/PostgresTestWorkDir/run/DemoPolicies/PostgresJDBC.policyxml
+
+ibmint deploy --input-path $PWD --output-work-directory /tmp/PostgresTestWorkDir --project JDBCApplication --project JDBCApplicationJava --project JDBCApplication_ComponentTest
+IntegrationServer -w /tmp/PostgresTestWorkDir --no-nodejs --start-msgflows no --test-project JDBCApplication_ComponentTest
 
 # Clean up JARs left by ibmint - git will notice if we leave them around; while
 # we could ignore JAR files with .gitignore, that would make it harder to upgrade
